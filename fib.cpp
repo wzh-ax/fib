6cc4f6c5c19404a740d8a3185d49007faf088dc3
2623752643 <2623752643@qq.com>
2023-07-29 15:51:54 +0800

    添加项目文件。

---
+++ fib.cpp
@@ -0,0 +1,20 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int fib(int n) {
+        vector<int> dp(n + 1);
+        if (n <= 1) {
+            return n;
+        }
+        int tmp;
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+};
